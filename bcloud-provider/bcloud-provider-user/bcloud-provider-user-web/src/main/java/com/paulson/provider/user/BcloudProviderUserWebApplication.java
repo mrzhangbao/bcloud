64b46180fafa08febe486abd2540926a634a7343
zhangbao2 <zhangbao@hua-cloud.com.cn>
2018-09-13 19:57:13 +0800

    新增服务提供者

---
+++ bcloud-provider/bcloud-provider-user/bcloud-provider-user-web/src/main/java/com/paulson/provider/user/BcloudProviderUserWebApplication.java
@@ -0,0 +1,12 @@
+package com.paulson.provider.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcloudProviderUserWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcloudProviderUserWebApplication.class, args);
+	}
+}
